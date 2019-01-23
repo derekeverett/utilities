a689515fdc7d29b9bf068d153a3adbe1d11bc1d8
everett.165 <everett.165@unity-1.asc.ohio-state.edu>
2019-01-23 18:02:51 -0500

    uses matplotlib

--- plot_ratio_of_block_files.py
+++ plot_ratio_of_block_files.py
@@ -4,25 +4,27 @@ import pandas as pd
 import math
 import sys
 import csv
+from matplotlib import cm
 
 file1 = sys.argv[1]
 file2 = sys.argv[2]
 
-#with open(file1, 'r') as csvfile:
-#    file1_data = csv.reader(csvfile, delimiter=' ')
-#    for row in file1_data:
-#        vals1.append(float(row[0]))
-
 df1 = pd.read_csv(file1, header=None, delimiter="\s+")
-
 df2 = pd.read_csv(file2, header=None, delimiter=r"\s+")
 
-#print(df1.values)
 
 ratio = df1.values / df2.values
-
-percent_diff = (df1.values - df2.values) / df1.values
+percent_diff = ( (df1.values - df2.values) / df1.values ) * 100
 
 np.savetxt('ratio_of_block_files.dat', ratio)
-
 np.savetxt('percent_diff_of_block_files.dat', percent_diff)
+
+plt.imshow(ratio, interpolation='nearest')
+plt.colorbar()
+plt.title("Ratio of file 1 to file 2")
+plt.show()
+
+plt.imshow(percent_diff, interpolation='nearest')
+plt.colorbar()
+plt.title("Percent Difference file 1 to file 2")
+plt.show()
